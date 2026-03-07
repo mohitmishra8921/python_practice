7856addd8c4abb2dacf765c4a7844d326bec32df
Mohit <mohitmishra8912@gmail.com>
2026-03-07 11:54:24 +0530

    hello

---
+++ DAY3/main1.py
@@ -0,0 +1,9 @@
+count = 0
+i = 12345
+while i>0:
+    i//=10
+    count += 1
+print(count)  
+ 
+
+
---
+++ DAY3/main2.py
@@ -0,0 +1,10 @@
+digit =0
+reverse = 0
+num = 1234
+while num<0:
+    num = num%10
+    digit = num
+    reverse = 0*digit + digit
+    # num+=1
+print(num)
+print(reverse)
