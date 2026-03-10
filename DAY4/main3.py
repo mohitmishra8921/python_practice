c8708dc831f1c1773d172170b3fd3d95408e22f5
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-10 15:30:40 +0530

    finding different ways to find multiples of any number

--- DAY4/main3.py
+++ DAY4/main3.py
@@ -1,13 +1,18 @@
 #exact problem for day 
-while True:
- num = int(input("Enter the number : "))
- for i in range (1,11):
-    print(num,"*",i,"=",num *i)
-
-
+try:
+   while True:
+     num = int(input("Enter the number : "))
+     for i in range (1,11):
+      print(num,"*",i,"=",num *i)
+except Exception as e:
+ print("Invalid input",e)
 
 #Another way to show previous problem
- while True:
-  num = int(input("Enter the number : "))
-  for i in range (1,11):
-    print(f"{num} * {i} = {num*i}")
+try:
+   while True:
+     num = int(input("Enter the number : "))
+     for i in range (1,11):
+      print(f"{num} * {i} = {num*i}")
+
+except Exception as e :
+  print("Invalid input",e)      
