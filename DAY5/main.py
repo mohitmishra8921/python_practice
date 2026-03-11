9972c41543bef88e91d1f40b1d2465f1979b3589
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-11 17:06:19 +0530

    new problem of day 5

---
+++ DAY5/main.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number : "))
+# last_digit = 0
+reverse = 0
+while num>0:
+    last_digit=  num%10
+    reverse = reverse*10+last_digit
+    num = num // 10
+print(reverse)    
