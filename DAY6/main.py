e5f2ad6289356c9bc4b19ba7cac80b7b0d1d24c6
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-12 17:26:41 +0530

    new commit of day 6

--- DAY6/main.py
+++ DAY6/main.py
@@ -7,6 +7,13 @@ while True:
             last_digit = num%10
             sum = sum + last_digit
             num = num//10
-        print(sum)
+        print("The sum of given digit by the user : ",sum)
        except Exception as e :
            print("invalid literal") 
+
+# Thinking Steps
+# Ask yourself:
+# 1️⃣ How can you get the last digit of a number?
+# 2️⃣ How will you add that digit to a sum variable?
+# 3️⃣ After taking the last digit, how will you remove it from the number?
+# 4️⃣ When should the loop stop?
