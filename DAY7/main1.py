2759a10ea031c81e47b1d067f15cc520be945acf
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-14 01:02:03 +0530

    new challenge for day 7

---
+++ DAY7/main1.py
@@ -0,0 +1,14 @@
+while True:
+       try:
+          number = int(input("Guess the secret number :\n"))
+          secret_number = 15
+          if(number == secret_number):
+            print("Yeah you guess right ✅ \n")
+            break
+          else:
+              print("Wrong guess ❌\n")      
+              print("Try again\n")
+       except Exception as e :
+                  
+        
+ 
