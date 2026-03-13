320ced2d4a25ecb490cca0b8afd4df64ec7f2a7d
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-14 01:06:26 +0530

    make better use of exception handling

--- DAY7/main.py
+++ DAY7/main.py
@@ -1,4 +1,31 @@
 
+# while True:
+#       try:  
+#             print("Simple Calculator \n")
+#             num1 = int(input("Enter the first number : "))
+#             num2 = int(input("Enter the second number : "))
+
+#             operator = input("Choose the following operators for calculation (*,/,-,+):  ")
+
+#             if(operator == "+"):
+#                print(f"The sum of num1 and num2 is : {num1+num2}")
+#             elif(operator == "-"):   
+#                 print(f"The difference of num1 and num2 is : {num1-num2}") 
+#             elif(operator == "/"):
+#                 if num2==0:
+#                     print("Division by zero is not possible\n")
+#                 else:
+#                     print(f"The quotient of num1 and num2 is : {num1/num2}") 
+#             elif(operator == "*"):
+#                 print(f"The product of num1 and num2 is : {num1*num2}") 
+#             else:
+#                 print("Please enter a valid operator (+ - * /)")  
+
+#       except Exception as e :
+#           print("Please enter valid numbers\n")
+
+
+
 while True:
       try:  
             print("Simple Calculator \n")
@@ -12,17 +39,14 @@ while True:
             elif(operator == "-"):   
                 print(f"The difference of num1 and num2 is : {num1-num2}") 
             elif(operator == "/"):
-                if num2==0:
-                    print("Division by zero is not possible\n")
-                else:
-                    print(f"The quotient of num1 and num2 is : {num1/num2}") 
+                print(f"The quotient of num1 and num2 is : {num1/num2}") 
             elif(operator == "*"):
                 print(f"The product of num1 and num2 is : {num1*num2}") 
             else:
                 print("Please enter a valid operator (+ - * /)")  
 
-      except Exception as e :
-          print("Please enter valid numbers\n")
+      except ZeroDivisionError :
+          print("Cannot divide by zero\n")          
 
                    
 
