d4a8f4125aecc67ca677e25a81382ec5d7262836
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-14 14:27:34 +0530

    new change

---
+++ DAY7/main3.py
@@ -0,0 +1,4 @@
+number = [5,10,15,20,25]
+print(number,"\n")
+for num in number :
+    print(num)
---
+++ DAY7/main4.py
@@ -0,0 +1,6 @@
+number = [2,4,6,8,77,10]
+largest = number[0]
+for num in number:
+    if num>largest:
+        largest = num
+print("Largest number of list :",largest)
