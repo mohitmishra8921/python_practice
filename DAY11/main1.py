475db70876fa5874aeff9b942205ede93681c674
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-16 17:40:49 +0530

    problem 1 of day 11

---
+++ DAY11/main1.py
@@ -0,0 +1,16 @@
+list_of_numbers = []
+
+user_input = int(input("How many numbers you want to add in a list?\n"))
+
+for num in range(user_input):
+    enter_numbers = int(input("Enter the numbers:\n"))
+    list_of_numbers.append(enter_numbers)
+
+print("Original list:", list_of_numbers)
+
+new_list = []
+
+for num in range(len(list_of_numbers) - 1, -1, -1):
+    new_list.append(list_of_numbers[num])
+
+print("Reversed list:", new_list)
