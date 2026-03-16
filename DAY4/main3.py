cd4f84648a5dee35829070f1c92d7480da0a26df
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-16 16:41:24 +0530

    make a little change in day 4 question

--- DAY4/main3.py
+++ DAY4/main3.py
@@ -3,11 +3,11 @@ try:
    while True:
      num = int(input("Enter the number : "))
      for i in range (1,11):
-      print(num,"*",i,"=",num *i)
+      print(num,"X",i,"=",num *i)
 except Exception as e:
  print("Invalid input",e)
 
-#Another way to show previous problem
+# Another way to show previous problem
 try:
    while True:
      num = int(input("Enter the number : "))
