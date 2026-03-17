cf729d6549b8fcbda1d5c8f3b4f596abb151d5c5
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-17 20:58:47 +0530

    change in my previous commit

--- DAY11/main2.py
+++ DAY11/main2.py
@@ -3,9 +3,9 @@ user_input = int(input("How many numbers you want to add in a list ?\n"))
 for i in range (user_input):
     enter_number = int(input("Enter numbers :\n"))
     list1.append(enter_number)
-print(list1)
+print("Original list : ",list1)
 unique_list = []
 for i in list1:
     if i not in unique_list:
         unique_list.append(i)                             
-print(unique_list)    
+print("This list contain unique value as compared to original list : ",unique_list)    
