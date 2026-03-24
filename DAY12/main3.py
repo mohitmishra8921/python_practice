4d1b28afc7548e75e8bf63c620143a3865456427
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-24 14:34:06 +0530

    last question of day 12

---
+++ DAY12/main3.py
@@ -0,0 +1,10 @@
+while True:
+    sentence = input("Write the sentence here: ")
+    split = sentence.split()
+    print(split)
+    print("Total words in the sentence :",len(split))
+# important things need to be understand
+# code needs to be working
+# structured of code is maintable
+# code readable
+# less line of code for less consumption of memory
