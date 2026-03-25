dff71ffd907c9a966badf8be6fbf8372ff047561
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-25 13:43:17 +0530

    question 1 of day 13

---
+++ DAY13/main.py
@@ -0,0 +1,6 @@
+
+num1 = int(input("Enter the first number:\n"))
+num2 = int(input("Enter the second number:\n"))
+def add(num1,num2):
+   return(f"The sum of num1 and num2 is :\n{num1 + num2}")
+print(add(num1,num2))
