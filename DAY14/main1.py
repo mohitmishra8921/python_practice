c5d6e0cad59075c194774e19324e5f01888fa869
Mohit Mishra <mohitmishra8921@gmail.com>
2026-03-28 00:59:06 +0530

    Qustion 2 of day 14

---
+++ DAY14/main1.py
@@ -0,0 +1,14 @@
+num = int(input("How many numbers you want to add in a list?\n"))
+list1 = []
+for num in range(num):
+    numbers = int(input("Enter numbers:\n"))
+    list1.append(numbers)
+print(list1)    
+def max_number(list1):
+    max_num = 0
+    for num in list1:
+        if num>max_num :
+            max_num = num
+    return max_num
+print(f"The maximum number of a list is :\n{max_number(list1)}")        
+
