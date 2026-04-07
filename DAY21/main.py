74b9b0ab232530fa7a17dfd97f3e97bb630a42c5
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-07 12:42:53 +0530

    first question of day 1

---
+++ DAY21/main.py
@@ -0,0 +1,11 @@
+list1 = [5,10,15]
+largest = 0
+second_largest = 0
+for i in list1:
+       if i>largest:
+            second_largest=largest
+            largest=i
+       elif i < largest and i > second_largest:
+          second_largest=i
+print("Largest digit of list:",largest)
+print("Second largest digit of list:",second_largest)        
