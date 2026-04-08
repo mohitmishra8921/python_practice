b84df8ccee03ff736fc3ed8f401c0951e77a721f
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-08 14:53:30 +0530

    last question of day 21

--- DAY21/main2.py
+++ DAY21/main2.py
@@ -1,9 +1,9 @@
-list1 = [2, 4, 3, 5, 7]
-target = 5
+list1 = [2, 4, 3, 5, 7, 9, 1, 6]
+target = 10
 
-for i in range(len(list1)):
-    for j in range(i+1, len(list1)):
-        if list1[i] + list1[j] == target:
-         print("The pairs which matches the target: ",(list1[i], list1[j]))
+for i in range(len(list1)):# This outer loops runs in form of row for this list length 
+    for j in range(i+1, len(list1)):# this outer loops run in form of columns fro the same length of list
+         if list1[i] + list1[j] == target: # This condition is checked using row and column created bu outer loop and inner loop
+          print(f"The pairs which matches the target: {(list1[i], list1[j])}")
 
         
