bb129d492da288e345139a77df26e5ae52b50a9a
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-09 19:12:36 +0530

    update previous code

--- DAy22/main.py
+++ DAy22/main.py
@@ -1,7 +1,7 @@
-list1 = [5,2,4,1]#when list is not sorted
+list1 = [5,4,1,6,2]#when list is not sorted
 for i in range(1,5):
     if i not in list1:
         print("The missing value in the list is : ",i)
         break
        
-    
+    #this code is correct for each edge case  i tried it in many ways
