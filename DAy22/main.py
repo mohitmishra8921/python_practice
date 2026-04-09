f6663a198d1d72e6a0fe91b675c6cd3ad37318b0
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-09 19:08:20 +0530

    update previous code

--- DAy22/main.py
+++ DAy22/main.py
@@ -1,7 +1,6 @@
-list1 = [1,2,4,5]#when list is sorted
-n= len(list1)+1
-for i in range(1,n+1):
-    if i not in  list1:
+list1 = [5,2,4,1]#when list is sorted
+for i in range(1,5):
+    if i not in list1:
         print("The missing value in the list is : ",i)
         break
        
