6d0d55ae4b7cacbd914e2c11c22f18ac012efe54
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-10 12:12:14 +0530

    update some small issue in code

--- DAy22/main2.py
+++ DAy22/main2.py
@@ -1,7 +1,6 @@
 list1 = [10,20,30,40,50]
 rotate_list = []
 for i in list1[-2:]:# Remeber one thing when work with lists than use index as their range 
-    if i>3:
         rotate_list.append(i)
 for i in list1[:3]:
             rotate_list.append(i)
