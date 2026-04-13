6771072b4dfc2c5129c816c4e10c0d0ec10e2947
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-13 23:02:24 +0530

    last question of day24

---
+++ DAY24/main2.py
@@ -0,0 +1,10 @@
+string1 = "listen"
+string2 = "silent"
+sorted(string1)
+sorted(string2)
+# print(sorted(string1))
+# print(sorted(string2))
+if sorted(string1)==sorted(string2):
+    print("Anagram")
+else:
+    print("Not A Anagram")
