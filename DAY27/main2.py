30f8687c49150314231bc45f33e671eeb2ea8dcb
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-21 13:07:55 +0530

    last question of day 27

---
+++ DAY27/main2.py
@@ -0,0 +1,13 @@
+list1 = [0,1, -2, 3, -4, 5]
+list2 = []
+for i in list1:
+    if i<0:
+        list2.append(i)
+for i in list1:
+    if i>=0:
+        list2.append(i)
+print(list2)        
+      
+
+
+        
