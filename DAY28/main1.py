9cf35f17755107ec01e509cb7e92e25ef986c288
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-24 00:00:02 +0530

    new approach second  question of day28

--- DAY28/main1.py
+++ DAY28/main1.py
@@ -1,18 +1,29 @@
-string1 = input("Enter the string: \n").split()
+# string1 = input("Enter the string: \n").split()
 
 
 
-new_string1 = ""
-reverse_string1 = " "
+# new_string1 = ""
+# reverse_string1 = " "
 
-for ch1 in string1:
-    if ch1 ==string1[0]:
-        new_string1 = (f'{ch1 + new_string1}')
-    else:
-        new_string1 = ch1 + new_string1
-        for ch in new_string1:
-            reverse_string1 = ch + reverse_string1  
-print(reverse_string1)             
+# for ch1 in string1:
+#     if ch1 ==string1[0]:
+#         new_string1 = (f'{ch1 + new_string1}')
+#     else:
+#         new_string1 = ch1 + new_string1
+#         for ch in new_string1:
+#             reverse_string1 = ch + reverse_string1  
+# print(reverse_string1) 
+# Another way of this problem
+string1 = input("Enter the string:\n").split()
+
+result = []
+
+for word in string1:
+    result.append(word[::-1])
+
+final = " ".join(result)
+
+print(final)            
                
 
 
