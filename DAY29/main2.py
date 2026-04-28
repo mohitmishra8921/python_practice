b2c5586561a19e3dd3638c2c1c0c7551640d6f9c
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-28 23:59:48 +0530

    improve Third question of day29

--- DAY29/main2.py
+++ DAY29/main2.py
@@ -1,16 +1,16 @@
-list1 = [1,2,3,4]
-list2 = [3,4,1,2]
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 1, 2]
 
-if len(list1)!=len(list2):
-   print("Not Rotated")
+# Step 1: Length check
+if len(list1) != len(list2):
+    print("Not Rotated")
+
 else:
-    new_list = list1+list1
-    if str(list2) in str(new_list):
-       print("Rotated")
+    # Step 2: Create combined list
+    combined_list = list1 + list1
+
+    # Step 3: Check rotation
+    if str(list2) in str(combined_list):
+        print("Rotated")
     else:
-       print("Not Rotated")
-        
-   
-
-
-    
+        print("Not Rotated")
