2c63b5ff37fd19cd1e3e846c1c3723dcec1e06e4
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-28 23:56:41 +0530

    improve Third question of day29

--- DAY29/main2.py
+++ DAY29/main2.py
@@ -1,11 +1,16 @@
 list1 = [1,2,3,4]
 list2 = [3,4,1,2]
-list3 = list1 + list1
-for i in list2:
-    if list2  in list3:
-        print("No it's not the rotated version of list1")
-        break
-else:
-    print("Yes list2 is rotated of list1")
 
+if len(list1)!=len(list2):
+   print("Not Rotated")
+else:
+    new_list = list1+list1
+    if str(list2) in str(new_list):
+       print("Rotated")
+    else:
+       print("Not Rotated")
         
+   
+
+
+    
