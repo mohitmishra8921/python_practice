e9575062c08dc209f2fc25a855b17edaecd3feae
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-29 09:42:58 +0530

    avoid duplicates in this method by adding one more condition with and logical operator

--- DAY30/main.py
+++ DAY30/main.py
@@ -1,7 +1,7 @@
-list1 = [1,2,3,4]
+list1 = [1,2,3,3,4]
 list2 = [3,4,5,6]
 common_elements =[]
 for i in list1:
-    if i in list2:
+    if i in list2 and i not in common_elements:
         common_elements.append(i)
 print(common_elements)
