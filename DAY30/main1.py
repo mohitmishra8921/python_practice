acc743620ca67d7ae5dddb91e9f5ad54b2419654
Mohit Mishra <mohitmishra8921@gmail.com>
2026-04-30 13:51:39 +0530

    Question 2 of day 30

--- DAY30/main1.py
+++ DAY30/main1.py
@@ -1,6 +1,9 @@
 string1 = ("I love programming").split()
 largest = ""
 for word in string1:
-    if len(word)>len(word[0]):
-        largest = len(word)
-print("Largest word of the sentence:\n",word)
+    if len(word) > len(largest):
+        largest = word
+print("Largest word of the sentence:\n",largest)
+        
+   
+    
